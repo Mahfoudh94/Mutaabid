1b6fcd8b1e00e1b6cee34a8e81db164356ef7d34
Mahfoudh94 <mouhamedali5@gmail.com>
2025-02-20 17:57:53 +0100

    Initial commit

--- app/src/main/java/net/rebaat/mutaabid/MainActivity.kt
+++ app/src/main/java/net/rebaat/mutaabid/MainActivity.kt
@@ -4,29 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import net.rebaat.mutaabid.presentation.view.MainScreen
-import net.rebaat.mutaabid.presentation.viewmodel.WirdViewModel
 import net.rebaat.mutaabid.ui.theme.MutaabidTheme
-import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val wirdViewModel: WirdViewModel by inject()
         enableEdgeToEdge()
         setContent {
             MutaabidTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                 ) { innerPadding ->
-                    MainScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        wirdViewModel = wirdViewModel
-                    )
+                    Box(
+                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("As-salaam Alaikum")
+                    }
                 }
             }
         }
